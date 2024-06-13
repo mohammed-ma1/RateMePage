f4a627196cf9da9930b7e825b7a2fe6af40adf16
mohammed-ma1 <56792970+mohammed-ma1@users.noreply.github.com>
2024-06-14 02:35:28 +0300

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
+import { RoutesKey } from './models/routes';
 
 export const routes: Routes = [
-  { path: ':id', component: MainComponent },
+  {
+    path: '',
+    redirectTo:RoutesKey.MainPage,
+    pathMatch: 'full',
+  },
+  {
+    path: RoutesKey.MainPage,
+    loadComponent: () => import('./pages/main/main.component').then( m => m.MainComponent)
+  },
+
 ];
 
 
