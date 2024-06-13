961f2f4d4ea6f3e6fa3d52d95674c6daa1d48be4
mohammed-ma1 <56792970+mohammed-ma1@users.noreply.github.com>
2024-06-14 02:31:23 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,43 @@
-import { importProvidersFrom } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient, HttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { RouterModule, Routes, provideRouter } from '@angular/router';
-import 'zone.js';
-import { AppComponent } from './app/app.component';
-import { ChttpService } from './app/services/chttp.service';
-import { MainComponent } from './app/pages/main/main.component';
-import { BillereService } from './app/pages/main/main.service';
-import { routes } from './app/app.routes';
-
-// Define your routes
+import { HttpClient, HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient } from "@angular/common/http";
+import { enableProdMode, importProvidersFrom } from "@angular/core";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { RouteReuseStrategy, provideRouter } from "@angular/router";
+import { TranslateService, TranslateStore, TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { AppComponent } from "./app/app.component";
+import { routes } from "./app/app.routes";
+import { CustomStorageService } from "./app/services/storage.service";
+import { CustomTranslateService } from "./app/services/translate.service";
+import { BillereService } from "./app/pages/main/main.service";
 
 
-// Factory function for the translation loader
-export function HttpLoaderFactory(httpClient: HttpClient) {
-  return new TranslateHttpLoader(httpClient);
+enableProdMode();
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
-// Bootstrap the application
+
+
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
+    TranslateService,
+    TranslateStore,
+    CustomStorageService,
+    CustomTranslateService,
     importProvidersFrom(
       TranslateModule.forRoot({
+        defaultLanguage: 'ar',
         loader: {
           provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
+          useFactory: createTranslateLoader,
           deps: [HttpClient],
-        },
+
+        }
       }),
     ),
-    provideAnimations(),
     BillereService,
     provideRouter(routes),
-
   ],
 });
