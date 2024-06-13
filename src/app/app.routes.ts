0614f93b9190428d2a925e23d3a74c1d4ac66ff4
mohammed-ma1 <56792970+mohammed-ma1@users.noreply.github.com>
2024-06-14 02:19:10 +0300

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,7 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home/1', pathMatch: 'full' },
-  { path: 'home/:id', component: MainComponent },
-  { path: '**', redirectTo: 'home/1', pathMatch: 'full' }
+
 ];
 
 
