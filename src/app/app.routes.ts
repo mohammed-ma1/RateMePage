f870143017112ddf58d5ba9342c83323f12cb363
mohammed-ma1 <56792970+mohammed-ma1@users.noreply.github.com>
2024-06-14 02:22:49 +0300

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 
 export const routes: Routes = [
-
+  { path: ':id', component: MainComponent },
 ];
 
 
