086074bb9c1ec21c0a297dd2b287768633e0f472
mohammed-ma1 <56792970+mohammed-ma1@users.noreply.github.com>
2024-06-14 03:22:56 +0300

    test

---
+++ dist/rate-me-web/browser/chunk-P5JW6R2Y.js
@@ -0,0 +1,1 @@
+import{s as a}from"./chunk-DUEH6RBV.js";export{a as MainComponent};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 import { RoutesKey } from './models/routes';
 
 export const routes: Routes = [
+  {
+    path: '/:id',
+    loadComponent: () => import('./pages/main/main.component').then( m => m.MainComponent)
+  },
 
 ];
 
